81c0febd38e8679cdbe23b92123063b5b230b1bb
Martin Bros <mbskot@gmail.com>
2015-11-06 12:12:14 +0100

    U - pridané logovanie chýbajúceho súboru vo FilePresenterCallback.

--- src/Brosland/Media/Callbacks/FilePresenterCallback.php
+++ src/Brosland/Media/Callbacks/FilePresenterCallback.php
@@ -4,6 +4,7 @@ namespace Brosland\Media\Callbacks;
 
 use Brosland\Media\IFileStorage,
 	Kdyby\Doctrine\EntityDao,
+	Nette\Application\BadRequestException,
 	Nette\Application\Responses\FileResponse;
 
 class FilePresenterCallback extends \Nette\Object implements \Brosland\Media\IFileCallback
@@ -32,6 +33,7 @@ class FilePresenterCallback extends \Nette\Object implements \Brosland\Media\IFi
 	/**
 	 * @param string $fileName
 	 * @return FileResponse
+	 * @throws BadRequestException
 	 */
 	public function __invoke($fileName)
 	{
@@ -39,14 +41,16 @@ class FilePresenterCallback extends \Nette\Object implements \Brosland\Media\IFi
 
 		if (!$file)
 		{
-			throw new \Nette\Application\BadRequestException('File not found', 404);
+			throw new BadRequestException('File not found', 404);
 		}
 
 		$path = $this->storage->getPath($file);
 
 		if (!file_exists($path))
 		{
-			throw new \Nette\Application\BadRequestException('File not found.', 404);
+			\Tracy\Debugger::log(new \Nette\InvalidStateException('File not found in the storage.'));
+
+			throw new BadRequestException('File not found.', 404);
 		}
 
 		$fileName = \Nette\Utils\Strings::webalize($file->getLabel()) . '.' . $file->getExt();
